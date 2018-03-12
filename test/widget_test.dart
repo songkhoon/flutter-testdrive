22ebfb09c38bd56461cceb2d0b1b6ba86a279371
Jeff <jeff.lim@compass-interactive.com>
2018-03-12 17:28:57 +0800

    Add User json

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,10 +4,20 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:testdrive/User.dart';
 
 import 'package:testdrive/main.dart';
+
+final json = """
+  {
+    "name": "John Smith",
+    "email": "john@example.com"
+  }
+  """;
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
@@ -25,5 +35,18 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+  });
+
+  test('Test Json', () {
+    print(json);
+    Map<String, dynamic> user = JSON.decode(json);
+    print("user name: ${user['name']}.");
+
+    var mapUser = new User.fromJson(JSON.decode(json));
+    print("user email: ${user['email']}");
+
+    var newUser = new User(mapUser.name, mapUser.email);
+    var newUserJson = newUser.toJson();
+    print(newUserJson);
   });
 }
