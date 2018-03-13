350b369ecef42e5d62db34d3caf7c696a317408e
Jeff <jeff.lim@compass-interactive.com>
2018-03-13 10:26:38 +0800

    Update User

--- lib/User.dart
+++ lib/User.dart
@@ -1,12 +1,31 @@
 class User {
   final String name;
   final String email;
+  String address;
+  String gender;
+  List<Car> cars;
 
   User(this.name, this.email);
 
   User.fromJson(Map<String, dynamic> json)
       : name = json['name'],
-        email = json['email'];
+        email = json['email'],
+        address = json['address'],
+        gender = json['gender'],
+        cars = (json['cars'] as List)?.map((car) => car == null ? null : new Car.fromJson(car))?.toList()
+  ;
 
-  Map<String, dynamic> toJson() => {'name': name, 'email': email};
+  Map<String, dynamic> toJson() => {'name': name, 'email': email, 'address': address};
+}
+
+class Car {
+  String name;
+  int year;
+
+  Car.fromJson(Map<String, dynamic> json)
+  :name = json["name"]
+  ;
+
+  Map<String, dynamic> toJson() => {'name': name};
+
 }
