48cd12ab7f8cdfc92250a6cc9b7d817bee81e141
Jeff <jeff.lim@compass-interactive.com>
2018-03-16 17:49:53 +0800

    Update animation page

--- lib/AnimationPage.dart
+++ lib/AnimationPage.dart
@@ -7,8 +7,7 @@ class AnimationPage extends StatefulWidget {
   }
 }
 
-class _AnimationState extends State<AnimationPage>
-    with SingleTickerProviderStateMixin {
+class _AnimationState extends State<AnimationPage> with SingleTickerProviderStateMixin {
   Animation<double> animation;
   AnimationController controller;
 
@@ -18,21 +17,28 @@ class _AnimationState extends State<AnimationPage>
     controller = new AnimationController(
         duration: const Duration(milliseconds: 2000), vsync: this);
 
-    animation = new Tween(begin: 0.0, end: 300.0).animate(controller)
-      ..addListener(() {
-        setState(() {});
+    animation = new Tween(begin: 0.0, end: 100.0).animate(controller)
+      ..addStatusListener((state) {
+        print("state: $state");
+        if (state == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (state == AnimationStatus.dismissed) {
+          controller.forward();
+        }
       });
+
     controller.forward();
   }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Animation Page"),
-      ),
-      body: _animationLogo(),
-    );
+        appBar: new AppBar(
+          title: new Text("Animation Page"),
+        ),
+        body: new Center(
+          child: _listView(),
+        ));
   }
 
   @override
@@ -41,15 +47,87 @@ class _AnimationState extends State<AnimationPage>
     super.dispose();
   }
 
-  Widget _animationLogo() {
-    return new Center(
-      child: new Container(
-        margin: new EdgeInsets.symmetric(vertical: 10.0),
-        height: animation.value,
-        width: animation.value,
-        child: new FlutterLogo(),
-      ),
+  Widget _listView() {
+    return new ListView(
+      children: <Widget>[
+        _animatedLogo(),
+        _animatedWidgetMethod(),
+        _growTransition(),
+      ],
     );
   }
 
+  // need to setState on addListener
+  Widget _animatedLogo() {
+    animation.addListener(() {
+      setState(() {});
+    });
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      height: animation.value,
+      width: animation.value,
+      child: new FlutterLogo(),
+    );
+  }
+
+  Widget _animatedWidgetMethod() {
+    return new AnimatedLogo(
+      animation: animation,
+    );
+  }
+
+  Widget _growTransition() {
+    return new GrowTransition(
+      child: new LogoWidget(),
+      animation: animation,
+    );
+  }
+}
+
+class AnimatedLogo extends AnimatedWidget {
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+    return new Container(
+      margin: new EdgeInsets.symmetric(vertical: 10.0),
+      height: animation.value,
+      width: animation.value,
+      child: new FlutterLogo(),
+    );
+  }
+}
+
+class LogoWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: new EdgeInsets.symmetric(vertical: 10.0),
+      child: new FlutterLogo(),
+    );
+  }
+}
+
+class GrowTransition extends StatelessWidget {
+  final Widget child;
+  final Animation<double> animation;
+
+  GrowTransition({this.child, this.animation});
+
+  @override
+  Widget build(BuildContext context) {
+    return new AnimatedBuilder(
+      animation: animation,
+      builder: (context, child) {
+        return new Container(
+          height: animation.value,
+          width: animation.value,
+          child: child,
+        );
+      },
+      child: child,
+    );
+  }
 }
