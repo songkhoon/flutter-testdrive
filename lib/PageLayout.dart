3544ed843a82b07a1ce3a26c14c86cc0e427de2c
Jeff <jeff.lim@compass-interactive.com>
2018-03-16 16:09:30 +0800

    Update PageLayout

--- lib/PageLayout.dart
+++ lib/PageLayout.dart
@@ -25,46 +25,56 @@ class PageLayoutState extends State<PageLayout> {
         title: new Text("Page Layout"),
       ),
       body: new Center(
-          child: new Stack(children: <Widget>[
-        new PageView(
-          children: <Widget>[_imageContainer, _imageContainer],
-        ),
-        new Positioned(
-            bottom: 30.0,
-            left: 0.0,
-            right: 0.0,
-            child: new Container(
-              child: new Column(
-                children: <Widget>[
-                  new RaisedButton(
-                    child: new Container(
-                      child: const Text(
-                        "Long Button Name 1",
-                        textAlign: TextAlign.center,
-                      ),
-                      width: MediaQuery.of(context).size.width / 2,
-                    ),
-                    color: Colors.orange,
-                    textColor: Colors.white,
-                    onPressed: () {},
-                  ),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  new RaisedButton(
-                    child: new Container(
-                      child: const Text(
-                        "Button 2",
-                        textAlign: TextAlign.center,
-                      ),
-                      width: MediaQuery.of(context).size.width / 2,
-                    ),
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-            )),
-      ])),
+          child: new Stack(
+              children: <Widget>[
+                _backgroundPage(),
+                _buttonContainer()
+              ])),
     );
+  }
+
+  Widget _backgroundPage() {
+    return new PageView(
+      children: <Widget>[_imageContainer, _imageContainer],
+      controller: new PageController(viewportFraction: 1.0),
+    );
+  }
+
+  Widget _buttonContainer() {
+    return new Positioned(
+        bottom: 30.0,
+        left: 0.0,
+        right: 0.0,
+        child: new Container(
+          child: new Column(
+            children: <Widget>[
+              new RaisedButton(
+                child: new Container(
+                  child: const Text(
+                    "Long Button Name 1",
+                    textAlign: TextAlign.center,
+                  ),
+                  width: MediaQuery.of(context).size.width / 2,
+                ),
+                color: Colors.orange,
+                textColor: Colors.white,
+                onPressed: () {},
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              new RaisedButton(
+                child: new Container(
+                  child: const Text(
+                    "Button 2",
+                    textAlign: TextAlign.center,
+                  ),
+                  width: MediaQuery.of(context).size.width / 2,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ));
   }
 }
