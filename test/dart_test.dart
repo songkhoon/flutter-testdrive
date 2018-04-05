2b740a11cfc1599530aa9c829175bf400d4fc16b
Jeff <jeff.lim@compass-interactive.com>
2018-04-05 15:58:05 +0800

    Update Dart Test

--- test/dart_test.dart
+++ test/dart_test.dart
@@ -2,16 +2,23 @@ import 'package:test/test.dart';
 
 void main() {
   test('Test Singleton', () {
-    var instanceA = new Singleton();
-    print(instanceA.index);
-    instanceA.index = 10;
-    var instanceB = new Singleton();
-    print(instanceB.index);
+    var instance1 = new Singleton();
+    print(instance1.index);
+    instance1.index = 10;
+    var instance2 = new Singleton();
+    print(instance2.index);
+
+    var instance3 = new Singleton.empty();
+    print(instance3.index);
+    instance3.index = 88;
+    var instance4 = new Singleton.empty();
+    print(instance4.index);
   });
 
   test('Test Maxins', () {
     var person = new Person();
     person.playMusic();
+    print("canPlayPiano: ${person.canPlayPiano}");
   });
 }
 
@@ -27,6 +34,10 @@ class Singleton {
     return _singleton;
   }
 
+  factory Singleton.empty() {
+    return new Singleton();
+  }
+
   Singleton._internal() {
     index = 100;
   }
@@ -39,7 +50,7 @@ class Musical {
   }
 }
 
-class Driver {
+abstract class Driver {
   bool canPlayPiano = false;
   playMusic() {
     print("no play");
@@ -49,7 +60,7 @@ class Driver {
 /*
 Maxins - To implement a mixin, create a class that extends Object, declares no constructors, and has no calls to super
  */
-class Person extends Object with Musical, Driver {
+class Person extends Object with Driver, Musical {
   Person() {
     print("Person");
   }
